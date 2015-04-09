ea2aad004f652a9c08504c85e285ba0358302a1f
gChenevert <grace.chenevert@selu.edu>
2015-04-09 13:51:53 -0500

    Login for mobile incomplete, but committing to be able to work on a different computer

--- CustomerConsumer/CustomerConsumer/MainActivity.cs
+++ CustomerConsumer/CustomerConsumer/MainActivity.cs
@@ -12,12 +12,20 @@ namespace CustomerConsumer
 	[Activity (Label = "CustomerConsumer", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
+		private Button loginBTN;
+
 		int count = 1;
 
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
+			loginBTN = FindViewById<Button> (Resource.Id.LoginBTN);
+			loginBTN.Click += (object sender, EventArgs e) => {
+				FragmentTransaction transaction = FragmentManager.BeginTransaction ();
+				DialogLogin loginDialog = new DialogLogin ();
+				loginDialog.Show (transaction, "dialog fragment");
+			};
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
@@ -28,6 +36,7 @@ namespace CustomerConsumer
 			button.Click += delegate {
 				button.Text = string.Format ("{0} clicks!", count++);
 			};
+
 		}
 	}
 }
