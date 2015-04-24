cdc4ef817c25886c1d4664b3907b164b3534b8bf
Grace.Chenevert <grace.chenevert@selu.edu>
2015-04-24 12:52:56 -0500

    Worked in mobile app. Set up cart to automatically generate a certain number of buttons and text fields. Also added total field and checkout button. Should be ready for the API to generate buttons/text.

--- CustomerConsumer/CustomerConsumer/CartActivity.cs
+++ CustomerConsumer/CustomerConsumer/CartActivity.cs
@@ -22,10 +22,42 @@ namespace CustomerConsumer
 
 			try{
 				SetContentView (Resource.Layout.Cart);
+				double total = 0;
+				int rand = new Random().Next(1, 70);
+				TableLayout layout = FindViewById<TableLayout>(Resource.Id.buttonLayoutCart);
+				for(int i = 1; i <= rand; ++i){
+					TableRow tRow = new TableRow(this);
+					layout.AddView(tRow);
+					Button button = new Button(this);
+					button.LayoutParameters = new TableRow.LayoutParams(
+						TableRow.LayoutParams.MatchParent,
+						TableRow.LayoutParams.WrapContent,
+						.5f);
+					tRow.AddView (button);
+					button.Text = string.Format("Game "+i);
+					TextView price = new TextView(this);
+					price.LayoutParameters = new TableRow.LayoutParams(
+						TableRow.LayoutParams.MatchParent,
+						TableRow.LayoutParams.WrapContent,
+						.5f);
+					price.TextSize = 20;
+					price.Gravity = GravityFlags.Right;
+					tRow.AddView (price);
+					total = total + (i*10+4.99);
+					price.Text = string.Format("$"+i+"4.99");
+				}
+				TextView totalText = FindViewById<TextView>(Resource.Id.totalForCart);
+				totalText.Text = string.Format("$"+total);
 			}
 			catch (Exception ex){
 				string x = ex.Message;
 			}
+		}
+		public void addButton(int i){
+
+		}
+		public void processClick(int i){
+			//do button stuff here
 		}
 	}
 }
--- CustomerConsumer/CustomerConsumer/Resources/Resource.designer.cs
+++ CustomerConsumer/CustomerConsumer/Resources/Resource.designer.cs
@@ -60,50 +60,68 @@ namespace CustomerConsumer
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f05000c
-			public const int GameBTN = 2131034124;
+			// aapt resource value: 0x7f050000
+			public const int CartText = 2131034112;
 			
-			// aapt resource value: 0x7f050009
-			public const int GameList = 2131034121;
+			// aapt resource value: 0x7f050012
+			public const int GameBTN = 2131034130;
 			
-			// aapt resource value: 0x7f05000a
-			public const int LoginBTN = 2131034122;
+			// aapt resource value: 0x7f05000f
+			public const int GameList = 2131034127;
 			
-			// aapt resource value: 0x7f050005
-			public const int LoginSubmitBTN = 2131034117;
-			
-			// aapt resource value: 0x7f050003
-			public const int Password = 2131034115;
-			
-			// aapt resource value: 0x7f050004
-			public const int PasswordText = 2131034116;
-			
-			// aapt resource value: 0x7f050001
-			public const int UserName = 2131034113;
-			
-			// aapt resource value: 0x7f050002
-			public const int UserNameText = 2131034114;
-			
-			// aapt resource value: 0x7f050008
-			public const int button1 = 2131034120;
-			
-			// aapt resource value: 0x7f05000e
-			public const int goToCart = 2131034126;
-			
-			// aapt resource value: 0x7f05000d
-			public const int goToGames = 2131034125;
+			// aapt resource value: 0x7f050010
+			public const int LoginBTN = 2131034128;
 			
 			// aapt resource value: 0x7f05000b
-			public const int outputText = 2131034123;
+			public const int LoginSubmitBTN = 2131034123;
+			
+			// aapt resource value: 0x7f050009
+			public const int Password = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int PasswordText = 2131034122;
 			
 			// aapt resource value: 0x7f050007
-			public const int searchGenre = 2131034119;
+			public const int UserName = 2131034119;
 			
-			// aapt resource value: 0x7f050000
-			public const int textView1 = 2131034112;
+			// aapt resource value: 0x7f050008
+			public const int UserNameText = 2131034120;
+			
+			// aapt resource value: 0x7f05000e
+			public const int button1 = 2131034126;
+			
+			// aapt resource value: 0x7f050001
+			public const int buttonLayoutCart = 2131034113;
+			
+			// aapt resource value: 0x7f050005
+			public const int checkout = 2131034117;
+			
+			// aapt resource value: 0x7f050014
+			public const int goToCart = 2131034132;
+			
+			// aapt resource value: 0x7f050013
+			public const int goToGames = 2131034131;
+			
+			// aapt resource value: 0x7f050011
+			public const int outputText = 2131034129;
+			
+			// aapt resource value: 0x7f050002
+			public const int relativeLayout1 = 2131034114;
+			
+			// aapt resource value: 0x7f05000d
+			public const int searchGenre = 2131034125;
 			
 			// aapt resource value: 0x7f050006
-			public const int textView3 = 2131034118;
+			public const int textView1 = 2131034118;
+			
+			// aapt resource value: 0x7f05000c
+			public const int textView3 = 2131034124;
+			
+			// aapt resource value: 0x7f050003
+			public const int textview = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int totalForCart = 2131034116;
 			
 			static Id()
 			{
