3fe715685495a6706a1cdc3b182de14ef0abd42f
Tyler Becker <tyler.becker@selu.edu>
2015-04-30 22:46:12 -0500

    Realized that the name for the image small-med.png was going to break the app so I renamed it.

--- CustomerConsumer/CustomerConsumer/Resources/Resource.designer.cs
+++ CustomerConsumer/CustomerConsumer/Resources/Resource.designer.cs
@@ -59,6 +59,12 @@ namespace CustomerConsumer
 			// aapt resource value: 0x7f020004
 			public const int med = 2130837508;
 			
+			// aapt resource value: 0x7f020005
+			public const int small = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int smallMed = 2130837510;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
