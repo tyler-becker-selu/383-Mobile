5957e4c811684017430e2d8986885f3d69d65b61
Grace.Chenevert <grace.chenevert@selu.edu>
2015-05-05 00:10:32 -0500

    final final fix

--- GamesTore/EmployeeClient/Models/Cart.cs
+++ GamesTore/EmployeeClient/Models/Cart.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace EmployeeClient.Models
     {
         public string URL { get; set; }
         public int ID { get; set; }
+        [DisplayName("Check out ready")]
         public bool CheckoutReady { get; set; }
         public int User_Id { get; set; }
         public List<GamesForCart> Games { get; set; }
--- GamesTore/EmployeeClient/Models/ViewModels/EditSaleViewModel.cs
+++ GamesTore/EmployeeClient/Models/ViewModels/EditSaleViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using EmployeeClient.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,9 @@ namespace EmployeeClient.ViewModels
     public class EditSaleViewModel
     {
         public int Id{get;set;}
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayName("Release Date")]
         public DateTime SaleDate{get;set;}
         public decimal Amount { get; set; }
         public List<SaleGameQuaninty> Games {get;set;}
